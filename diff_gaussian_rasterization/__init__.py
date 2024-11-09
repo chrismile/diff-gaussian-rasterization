Fixed backwards pass for gradients.

diff_gaussian_rasterization/__init__.py
@@ -90,7 +90,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         return color, radii, gradients, invdepths
 
     @staticmethod
-    def backward(ctx, grad_out_color, _, grad_out_depth):
+    def backward(ctx, grad_out_color, _1, _2, grad_out_depth):
 
         # Restore necessary values from context
         num_rendered = ctx.num_rendered